7fbc3069f896e639c0cc79224c4b6c5bf172d2ee
karolck <karolck@gmail.com>
2015-06-16 22:02:20 +0200

    function getEntry

--- data.php
+++ data.php
@@ -36,11 +36,11 @@ function addEntry($title, $author, $page, $content){
 	
 	$file = fopen(Entries, 'a');
 	
-	$data = array(0 => base64_encode(htmlspecialchars($tytul)),
- 			base64_encode(htmlspecialchars($autor)),
+	$data = array(0 => base64_encode(htmlspecialchars($title)),
+ 			base64_encode(htmlspecialchars($author)),
  			time(),
- 			base64_encode(htmlspecialchars($www)),
- 			base64_encode(nl2br(htmlspecialchars($tresc)))			
+ 			base64_encode(htmlspecialchars($page)),
+ 			base64_encode(nl2br(htmlspecialchars($content)))			
 	);
 	
 	fwrite($file, implode('|', $data)."\r\n");
@@ -49,5 +49,34 @@ function addEntry($title, $author, $page, $content){
 	
 	//zmiana HTML na zwyk³y tekst
 	$content = htmlspecialchars($content);
+	$content = nl2br($content);
+	$content = base64_encode($content);
+	
+	//pobieranie wpisów, najpierw odwracamy tablicê tak aby najnowsze wpisy by³y na pocz¹tku
+	// nastêpnie dekodujemy dane i pakujemmy wszystko do tablicy rezult
+	
+	
+	function getEntry(){
+		$entries = array_reverse(file(Entries));
+		
+		$i =1;
+		$rezult = array();
+		
+		foreach ($entries as $entry){
+			$entry = explode('|', trim($entry));
+			$rezult [] = array(
+					'id' => $i,
+					'title' => base64_decode($entry[0]),
+					'author' => base64_decode($entry[1]),
+					'date' => date('d.m.Y, H:i', $entry[2]),
+					'page' => base64_decode($entry[3]),
+					'content' => base64_decode($entry[4])
+			);
+			$i++;
+		}
+		return $rezult;
+		
+	}
+	
 	
 }
--- index.php
+++ index.php
@@ -1,1 +1,62 @@
+<!DOCTYPE html 
+	PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+   <head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+   <title>Guest book</title>
+   </head>
+   <body>
+   	<h1>Guest book</h1>
+
 <?php
+	require ('./data.php');
+	
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		if(addEntry($_POST['title'], $_POST['author'],  $_POST['page'], $_POST['content'])){
+			echo 'The entry wos added';
+		}else {
+			echo 'Please fill out the form correctly';
+		}
+		echo '<p><a href="index.php">Back</a></p>';
+	}else {
+		$entries = getEntry();
+		foreach ($entries as $entry){
+			echo '<hr /><p><b>Title: <i>'.$entry['title'].'</i>; 
+ 				Author: '.$entry['author'].'; Date: '.$entry['date'];
+ 			if(strlen($entry['page']) > 0)
+ 			{
+ 				echo '; <a href="'.$entry['page'];
+ 			}
+ 			echo '</b></p>';
+ 			echo '<p>'.$entry['content'].'</p>';
+		}
+	}
+	
+	
+	?>
+	
+	<form method="post" action="index.php">
+		<table border="0" width="50%">
+			<tr>
+				<td>Title</td>
+				<td><input type="text" name="title"/></td>
+			</tr>
+			<tr>
+				<td>Author</td>
+				<td><input type="text" name="author"/></td>
+			</tr>
+			<tr>
+				<td>Page</td>
+				<td><input type="text" name="page"/></td>
+			</tr>
+			<tr>
+				<td>Content</td>
+				<td><textarea name="content" rows="4" cols="50"></textarea></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Add" /></td>
+			</tr>
+		</table>
+	</form>
